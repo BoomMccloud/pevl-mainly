cedf88b79939a26fcd4551cbc8f46eacb6dfa788
BoomMccloud <144580327+BoomMccloud@users.noreply.github.com>
2024-01-26 13:11:32 +0800

    Text changes for Pool Card

--- src/components/MainPane/components/PoolCard.tsx
+++ src/components/MainPane/components/PoolCard.tsx
@@ -143,23 +143,19 @@ export const PoolCard = ({ pool, currentPhase }: PoolStateType) => {
                 // Render a countdown
                 return (
                   <Heading as="h1" className="countdown" style={{ fontSize: 20 }}>
-                    🔥 {"Ends In: "}
+                    {"Ends In: "}
                     <span className="number">{zeroPad(hours)}</span>
-                    <span className="label">Hours</span>
-                    {" : "}
+                    <span className="label"> Hours</span>
+                    {" "}
                     <span className="number">{zeroPad(minutes)}</span>
-                    <span className="label">Minutes</span>
-                    {" : "}
-                    <span className="number">{zeroPad(seconds)}</span>
-                    <span className="label">Seconds</span>
-                    🔥
+                    <span className="label"> Minutes</span>
                   </Heading>
                 );
               }
             }}
           ></Countdown>
 
-          <Text>Choose the number to tickets to buy</Text>
+          <Text>Number of Tickets</Text>
 
           <HStack mb={2}>
             <Button {...dec}>-</Button>
@@ -175,7 +171,7 @@ export const PoolCard = ({ pool, currentPhase }: PoolStateType) => {
               onClick={handleTransfer}
               isLoading={isLoading || isPending}
             >
-              Buy tickets
+              Enter The Drawing!
             </Button>
           ) : (
             <Box display="flex" justifyContent="center">
@@ -183,6 +179,7 @@ export const PoolCard = ({ pool, currentPhase }: PoolStateType) => {
             </Box>
           )}
         </Stack>
+        <Text>Testnet PEVL points can win real Blast points. The raffle is verifiably random.</Text>
       </CardBody>
     </Card>
   );
